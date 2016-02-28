bd44af73fb02c650ea4e4c6cdde3cdb35b329d3c
jk993 <jk993@drexel.edu>
2016-02-27 22:09:14 -0500

    fix bug in js

--- jsGame/js/custom.js
+++ jsGame/js/custom.js
@@ -1,28 +1,28 @@
 var main = function() {
-    "use strict";
+    'use strict';
 
     var createUserName = function() {
-        var urname = $("#username").val();
+        var urname = $('#username').val();
         $.ajax({
-            method: "PUT",
-            url: "/users",
+            method: 'PUT',
+            url: '/users',
             data: { username:uname },
             statusCode: {
                 201: function() {
-                    $("#result_area").html("User successfully created.");
+                    $('#result_area').html('User successfully created.');
                 },
                 409: function() {
-                    $("#result_area').html('user already exists. Please try again.");
+                    $('#result_area').html('user already exists. Please try again.');
                 }
             }
         }
     }
 
-    $('#username').on("click", function(event) {
+    $('#username').on('click', function(event) {
         createUserName();
     });
 
-    $('#username').on("click", function(event) {
+    $('#username').on('click', function(event) {
         // when user press enter
         if(event.keyCode === 13) {
             createUserName();
