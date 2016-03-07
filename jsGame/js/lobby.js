58ae84ff9fcfd7f25795a612760a4d8c489bfac5
rawhat <alex41290@gmail.com>
2016-03-07 01:10:06 -0500

    testing more invites

--- jsGame/js/lobby.js
+++ jsGame/js/lobby.js
@@ -8,7 +8,12 @@ $(document).ready(function(event){
 
 	ws.onmessage = function(evt){
 		var status = JSON.parse(evt.data);
-		console.log("server: " + status.sender);
+		if(status.request){
+			console.log("request: " + status.request);
+		}
+		else if(status.sender){
+			console.log("server: " + status.sender);
+		}
 
 		// if failed to invite
 		if(status.status === "failed" &&
