3a384867b5b6f8188d3502b29b7ee5a1225556f1
Anh Le <aql25@drexel.edu>
2016-03-06 22:25:45 -0500

    testing socket

--- jsGame/js/lobby.js
+++ jsGame/js/lobby.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(event){
-	
+
+	var ws = new WebSocket("ws://http://192.168.1.238:8080/invite");
+
 	$.ajax({
 		method: "GET",
 		url: "/users"
@@ -10,19 +12,38 @@ $(document).ready(function(event){
 		console.log(json);
 		for(var i = 0; i < json.users.length; i++){
 			var user = json.users[i];			
-			addRow(i, user.username, user.wins, user.losses, user.rating);			
+			addRow(i, user.username, user.wins, user.losses, user.rating, ws);			
 		}
 	}).fail(function(){
+		//To do
+		// display error on screen
 		console.log("get no data");
 	});
 
 });
 
-function addRow(counter, username, win, loss, rate){
+function inviteHandler(button, socket){
+	socket.onopen = function(){
+
+		// send invitation to server
+		socket.send(JSON.stringify({
+			"function": "send",
+			"target": button.value
+		}));
+
+		// retrieve message
+		socket.onmessage = function(msg){
+			console.log(msg);
+		};
+	};
+	
+}
+
+function addRow(counter, username, win, loss, rate, fnc, socket){
 	var that = this;
-	var btn = createInviteButton(username, function(button){
-		console.log(button.value);
-	});
+	var btn = createInviteButton(username, that.inviteHandler, socket);
+
+
 	var index = createText("td", counter+1);
 	var user = createText("td", username);
 	var w = createText("td", win);
@@ -41,16 +62,47 @@ function createText(element, text){
 	return element;
 }
 
-function createInviteButton(username, func){
+function createInviteButton(username, func, socket){
 	var element = document.createElement("td");
     var button = document.createElement("button");
     var textNode = document.createTextNode("Invite");
     button.type = "button";
     button.value = username;
     button.appendChild(textNode);
-    button.onclick = func(button);
+    button.onclick = func(button, socket);
 
     element.appendChild(button);
     return element;
 }
 
+/*ws.onmessage = function(event){
+          var response = $.parseJSON(event.data);
+          console.log(response);
+          if(response.function == "joining_game"){
+            window.location.replace("/game/" + response.gameID);
+          }
+          else if(response.function == "create_game"){
+            $.ajax({
+              method: "PUT",
+              url: "/game",
+              data: {
+                "player2": currentInvite,
+              },
+            }).done(function(data){
+                window.location.replace("/game/" + $.parseJSON(data).gameID);
+            });
+          }
+          if(response.status !== undefined){
+            console.log(response.status);
+          }
+          if(response.request !== undefined){
+            console.log(response.request);
+          }
+
+          if(response.sender !== undefined){
+            $('#invite_area').show();
+            $('#invitation_text_area').text(response.sender + " has invited you to play!");
+            currentInvite = response.sender;
+            inviteButtonListeners();
+          }
+      }*/
