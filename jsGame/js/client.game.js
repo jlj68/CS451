05ff9ef854c38989392ed58a3d64489a647405e4
Anh Le <aql25@drexel.edu>
2016-03-07 15:14:10 -0500

    set up the game socket

--- jsGame/js/client.game.js
+++ jsGame/js/client.game.js
@@ -1,9 +1,33 @@
 var chess = {};
+var forfeitBtn = $('#forfeit-btn');
 
 window.onload = function(){
 	
 	var ws = new WebSocket("ws://subsonic.rawhat.net:8080/game/socket");
 	chess = new GameLogic(ws);
+	
+	ws.onopen = function(evt){
+		console.log("socket connected");
+	};
+	ws.onclose = function(evt){
+		console.log("socket closed");
+	};
+
+	ws.onmessage = function(msg){
+		
+	};
+
+	forfeitBtn.click(function(){
+		// show modal to ask for forfeit
+		$('#forfeitModal').modal('show');
+
+		$('#forfeitConfirmBtn').click(function(){
+			ws.send(JSON.stringify({
+	                'function': 'forfeit'
+	        }));
+		});
+		
+	});
 
 
 
